3c884b83e9f1ddc774ad8db390091eeeee692d00
LeoGog <levani0722@outlook.com>
2023-06-14 16:07:14 +0400

    16 ✅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import myImg from './assets/myImg.jpg'
+import pic1 from './assets/linkedin.svg'
+import pic2 from './assets/github.svg'
+import pic3 from './assets/codewars.svg'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="card-container">
+        <img className="round" src={myImg} alt="avatar" />
+        <p><span className="name">Levan Alimbarashvili</span></p>
+        <p><span className="address">Tbilisi, Georgia</span></p>
+        <p>Front-End Developer</p>
+        <a href="https://www.linkedin.com/in/levan-alimbarashvili-195539279/" class="icon-btn linkedin">
+            <img src={pic1} alt="linkedin icon" />
         </a>
-      </header>
+        <a href="https://github.com/Leo-Gog" className="icon-btn github">
+            <img src={pic2} alt="github icon" />
+        </a>
+        <a href="https://www.codewars.com/users/LeoGog" className="icon-btn codewars">
+            <img src={pic3} alt="codewars icon" />
+        </a>
+        <div className="skills">
+            <p><b>Skills:</b></p>
+            <ul>
+                <li>Front End Development</li>
+                <li>HTML 5</li>
+                <li>CSS 3</li>
+                <li>JavaScript</li>
+                <li>React (Basics)</li>
+                <li>PHP (Basics)</li>
+            </ul>
+        </div>
     </div>
   );
 }
